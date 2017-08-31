f033f738a96b9ae203391911ce1b1c85f87c2362
Ignas Grigaravicius <kickeris@gmail.com>
2017-08-31 13:08:12 +0100

    Board on screen

--- custom.js
+++ custom.js
@@ -27,3 +27,23 @@ function init(){
 	createSnake();
 	createFruit();
 }
+
+//map for the snake
+function createMap(){
+	document.write("<table>");
+
+	for ( var y = 0; y < height; y++){
+		document.write("<tr>");
+		for (var x = 0; x < width; x++){
+			if (x == 0 || x == width -1 || y == 0 || y== height -1){
+				document.write("<td class='wall' id='"+ x + "-" + y +"'></td>");
+			}else{
+				document.write("<td class='blank' id='"+ x + "-" + y +"'></td>");
+			}
+		}
+		document.write("<tr>");
+	}
+	document.write("</table>");	
+}
+
+run();
